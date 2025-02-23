7fc8ee102a8e2fe2444d788ae9ee243134f47a19
Pedro <pedrohvalles7@gmail.com>
2025-02-23 13:09:05 -0300

    Commit

---
+++ Core/Extensions/PreviewProvider.swift
@@ -0,0 +1,8 @@
+//
+//  PreviewProvider.swift
+//  CriptoApp
+//
+//  Created by Pedro Henrique Roca Moreira on 17/02/25.
+//
+
+import Foundation
---
+++ Core/Home/CoinRowView.swift
@@ -0,0 +1,18 @@
+//
+//  CoinRowView.swift
+//  CriptoApp
+//
+//  Created by Pedro Henrique Roca Moreira on 16/02/25.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoinRowView()
+}
